8ada84a7a2ba15f4667ecb46b7253ef55197fc66
Noor E Mustafa <135613158+NooredeenAjaj@users.noreply.github.com>
2023-07-03 13:57:09 +0200

    Update boot.py

--- src/boot.py
+++ src/boot.py
@@ -1,31 +1,33 @@
-def do_connect():
-    import network
-    from time import sleep
-    from secrets import secrets
-    import machine
+import network
+from time import sleep
+from secrets import secrets
+import machine
 
-    wlan = network.WLAN(network.STA_IF)        
 
-    if not wlan.isconnected():                 
-        print('connecting to network...')
-        wlan.active(True)                     
-       
-        wlan.config(pm = 0xa11140)
-        wlan.connect(secrets["ssid"], secrets["password"]) 
-        print('Waiting for connection...', end='')
-       
+def connect():
+
+
+    wlan = network.WLAN(network.STA_IF)  # Put modem on Station mode
+
+    if not wlan.isconnected():  # Check if already connected
+        print("connecting to network...")
+        wlan.active(True)  # Activate network interface
+        # set power mode to get WiFi power-saving off (if needed)
+        wlan.config(pm=0xA11140)
+        wlan.connect(secrets["ssid"], secrets["password"])  # Your WiFi Credential
+        print("Waiting for connection...", end="")
+        # Check if it is connected otherwise wait
         while not wlan.isconnected() and wlan.status() >= 0:
-            print('.', end='')
+            print(".", end="")
             sleep(1)
- 
+    # Print the IP assigned by router
     ip = wlan.ifconfig()[0]
-    print('\nConnected on {}'.format(ip))
-    return ip 
+    print("\nConnected on {}".format(ip))
+    return ip
 
 
-
+# WiFi Connection
 try:
-    ip = do_connect()
+    ip = connect()
 except KeyboardInterrupt:
     print("Keyboard interrupt")
-
