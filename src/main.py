0a0fbdb7f6e3db354c124f9ced988c21b7b00f59
Noor E Mustafa <135613158+NooredeenAjaj@users.noreply.github.com>
2023-07-03 13:55:21 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
-import time 
+import time  
 from mqtt import MQTTClient  
 import ubinascii  
 import machine  
 import micropython  
 import random  
-from machine import Pin 
+from machine import Pin  
 import network
+from boot import  connect
 
 
 
 
-from tempsensor import get_temp_humidity
+from tempsensor import get_temp
 from lightsensor import check_light
 from magnetdetected import is_magnet_detected
-
 
 
 led = Pin("LED", Pin.OUT)  
@@ -25,10 +25,12 @@ WIFI_PASS = "dtm2nxum"
 
 
 
+
 AIO_SERVER = "io.adafruit.com"
 AIO_PORT = 1883
 AIO_USER = "noorMustafa"
-AIO_KEY = "aio_zXiQ3552PEbFzlTycPnyp2Ek3gDV"
+AIO_KEY = "aio_oybT105q6s6O3wzZk5kq2wO63iph"
+
 AIO_CLIENT_ID = ubinascii.hexlify(machine.unique_id())  
 AIO_LIGHTS_FEED = "noorMustafa/feeds/lighting"
 AIO_TEMP_FEED = "noorMustafa/feeds/temperature"
@@ -39,41 +41,8 @@ AIO_DOOR_FEED = "noorMustafa/feeds/door"
 
 
 
-
-
-
-
-
-
-
-
-
-# Function to connect Pico to the WiFi
 def do_connect():
-
-    
-    import network
-    from time import sleep
-    import machine
-
-    wlan = network.WLAN(network.STA_IF)  
-
-    if not wlan.isconnected():  
-        print("connecting to network...")
-        wlan.active(True)  
-       
-        wlan.config(pm=0xA11140)
-        wlan.connect(WIFI_SSID, WIFI_PASS) 
-        print("Waiting for connection...", end="")
-       
-        while not wlan.isconnected() and wlan.status() >= 0:
-            print(".", end="")
-            sleep(1)
-
-    ip = wlan.ifconfig()[0]
-    print("\nConnected on {}".format(ip))
-    return ip
-
+    return connect()
 
 
 
@@ -84,7 +53,7 @@ def do_connect():
 
 def send_tempsensor_data():
 
-    temp, hum = get_temp_humidity()
+    temp = get_temp()
     
     try:
         client.publish(topic=AIO_TEMP_FEED, msg=str(temp))
@@ -114,40 +83,41 @@ def send_tempsensor_data():
 
 
 
-def sub_cb(topic, msg):  
-    print((topic, msg)) 
-    if msg == b"ON":  
+def sub_cb(topic, msg): 
+    print((topic, msg))  
+    if msg == b"ON": 
         led.on()  
     elif msg == b"OFF": 
         led.off() 
     else:  
-        print("Unknown message") 
+        print("Unknown message")  
 
 
 
 
 
 
-
+# Try WiFi Connection
 try:
     ip = do_connect()
 except KeyboardInterrupt:
     print("Keyboard interrupt")
 
-
+# Use the MQTT protocol to connect to Adafruit IO
 client = MQTTClient(AIO_CLIENT_ID, AIO_SERVER, AIO_PORT, AIO_USER, AIO_KEY)
 
-
+# Subscribed messages will be delivered to this callback
 client.set_callback(sub_cb)
 client.connect()
 client.subscribe(AIO_LIGHTS_FEED)
 print("Connected to %s, subscribed to %s topic" % (AIO_SERVER, AIO_LIGHTS_FEED))
 
 
-try:  
-
-    while 1:  
-        client.check_msg()  
+try:  # Code between try: and finally: may cause an error
+    # so ensure the client disconnects the server if
+    # that happens.
+    while 1:  # Repeat this loop forever
+        client.check_msg()  # Action a message if one is received. Non-blocking.
  
         send_tempsensor_data()
         print(check_light())
@@ -162,7 +132,7 @@ try:
 
 
         time.sleep(8)
-finally: 
-    client.disconnect()  
+finally:  # If an exception is thrown ...
+    client.disconnect()  # ... disconnect the client and clean up.
     client = None
     print("Disconnected from Adafruit IO.")
