5f4f392a5cf8198de368a8afc40f7df46176167a
Noor E Mustafa <noor_eidirhn.mustafa6827@student.lu.se>
2023-11-10 17:23:59 +0100

    dsfjasdf

--- src/main.py
+++ src/main.py
@@ -1,26 +1,22 @@
-import time  
-from mqtt import MQTTClient  
-import ubinascii  
-import machine  
-import micropython  
-import random  
-from machine import Pin  
+import time
+from mqtt import MQTTClient
+import ubinascii
+import machine
+import micropython
+import random
+from machine import Pin
 import network
-from boot import  connect
+from boot import connect
 
 
-
+# sadfjsadffdasklfjsaldk
 
 from tempsensor import get_temp
 from lightsensor import check_light
 from magnetdetected import is_magnet_detected
 
 
-led = Pin("LED", Pin.OUT)  
-
-
-
-
+led = Pin("LED", Pin.OUT)
 
 
 AIO_SERVER = "io.adafruit.com"
@@ -28,34 +24,20 @@ AIO_PORT = 1883
 AIO_USER = "yourname"
 AIO_KEY = "ad"
 
-AIO_CLIENT_ID = ubinascii.hexlify(machine.unique_id())  
+AIO_CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 AIO_LIGHTS_FEED = "noorMustafa/feeds/lighting"
 AIO_TEMP_FEED = "noorMustafa/feeds/temperature"
 AIO_DOOR_FEED = "noorMustafa/feeds/door"
 
 
-
-
-
-
-
-
-
-
-
-
-def sub_cb(topic, msg): 
-    print((topic, msg))  
-    if msg == b"ON": 
-        led.on()  
-    elif msg == b"OFF": 
-        led.off() 
-    else:  
-        print("Unknown message")  
-
-
-
-
+def sub_cb(topic, msg):
+    print((topic, msg))
+    if msg == b"ON":
+        led.on()
+    elif msg == b"OFF":
+        led.off()
+    else:
+        print("Unknown message")
 
 
 # Try WiFi Connection
@@ -74,23 +56,16 @@ client.subscribe(AIO_LIGHTS_FEED)
 print("Connected to %s, subscribed to %s topic" % (AIO_SERVER, AIO_LIGHTS_FEED))
 
 
-try: 
-    while 1:  
-        client.check_msg() 
- 
+try:
+    while 1:
+        client.check_msg()
+
         client.publish(topic=AIO_TEMP_FEED, msg=str(get_temp()))
-        client.publish(topic=AIO_LIGHTS_FEED , msg=str(check_light()))
-        client.publish(topic=AIO_DOOR_FEED , msg=str(is_magnet_detected()))
-
-        
-
-
-    
-
-
+        client.publish(topic=AIO_LIGHTS_FEED, msg=str(check_light()))
+        client.publish(topic=AIO_DOOR_FEED, msg=str(is_magnet_detected()))
 
         time.sleep(8)
-finally: 
+finally:
     client.disconnect()
     client = None
     print("Disconnected from Adafruit IO.")
